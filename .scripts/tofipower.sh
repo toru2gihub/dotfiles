5d9c8082a2f37e698a881b0e8034c1852b87ba2a
toru <toru777average@cock.li>
2024-06-19 09:59:11 -0400

    addedm doom thems (again), and change dmenu scripts to tofi, also, some hyprland changes

---
+++ .scripts/tofipower.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# ▀█▀▄▀▄█▀▄█ █
+#  █ ▀▄▀█▀▄▀▄█
+#  simple, clean, elegant
+#
+# Page: https://toru.codeberg.page/
+# Mastodon: @averagetiger89
+# Repository: https://codeberg.org/toru
+# Email: torupublic11@waifu.club
+
+ #  _         __ _                              
+ # | |_ ___  / _(_)_ __   _____      _____ _ __ 
+ # | __/ _ \| |_| | '_ \ / _ \ \ /\ / / _ \ '__|
+ # | || (_) |  _| | |_) | (_) \ V  V /  __/ |   
+ #  \__\___/|_| |_| .__/ \___/ \_/\_/ \___|_|   
+ #                |_|                           
+
+###################################################
+# control del poder del sistema con tofi+systemd #
+###################################################
+
+#dm="dmenu -i -c -l 4"
+#dm="dmenu -l 4"
+dm="tofi"
+
+# Options
+shutdown="shutdown"
+suspend="suspend"
+reboot="reboot"
+logout="exit"
+
+chosen=$(printf "$shutdown\n$reboot\n$logout\n$suspend" | $dm --output "Nixos-Session:")
+case $chosen in
+
+    $shutdown)
+  notify-send  "El systema se apagara en...." "$( echo "15 segundos" )" &
+  sleep 5
+  notify-send  "El systema se apagara en...." "$( echo "10 segundos" )" &
+  sleep 5 
+  notify-send  "El systema se apagara en...." "$( echo "5 segundos" )" &
+  sleep 4
+  su root -c systemctl poweroff 
+
+        ;;
+    $reboot)
+   su root -c  reboot
+        ;;
+
+    $logout)
+  pkill Hyprland
+        ;;
+
+    $suspend)
+  systemctl suspend && hyprlock
+        ;;
+esac
