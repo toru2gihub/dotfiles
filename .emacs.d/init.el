6efcc12975d0e8ecf48426f2ce1aebf19f56fb0a
toru <toru777average@cock.li>
2024-06-20 01:56:51 -0400

    change some hyprland animations, improve corfu and added his icons, and some minor changes to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,12 +42,12 @@
  '(org-safe-remote-resources
    '("\\`https://github\\.com/fniessen/org-html-themes/blob/master/org/theme-readtheorg\\.setup\\'"))
  '(package-selected-packages
-   '(doom-themes org-auto-tangle eglot magit embark-consult cdlatex auctex calfw-org solaire-mode ef-themes simple-modeline vundo evil-goggles org-roam-ui major-mode-hydra consult-org-roam avy consult-yasnippet org-download org-yaap nix-mode all-the-icons-dired org-roam highlight-indent-guides corfu cape modus-themes org-bullets dashboard unicode-fonts page-break-lines auto-complete no-littering olivetti rainbow-mode))
+   '(kind-icon yasnippet-capf nerd-icons-corfu corfu-doc doom-themes org-auto-tangle eglot magit embark-consult cdlatex auctex calfw-org solaire-mode ef-themes simple-modeline vundo evil-goggles org-roam-ui major-mode-hydra consult-org-roam avy consult-yasnippet org-download org-yaap nix-mode all-the-icons-dired org-roam highlight-indent-guides corfu cape modus-themes org-bullets dashboard unicode-fonts page-break-lines auto-complete no-littering olivetti rainbow-mode))
  '(simple-modeline-segments
    '((simple-modeline-segment-modified simple-modeline-segment-buffer-name simple-modeline-segment-position)
      (simple-modeline-segment-vc simple-modeline-segment-misc-info simple-modeline-segment-process simple-modeline-segment-major-mode)))
  '(telega-server-libs-prefix "/nix/store")
- '(toc-org-max-depth 3)
+ '(toc-org-max-depth 3 t)
  '(warning-suppress-types '((use-package)))
  '(yas-snippet-dirs '("/home/toru/.emacs.d/snippets/")))
 (custom-set-faces
