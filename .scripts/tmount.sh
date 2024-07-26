7ba8a83add04b3ca1eb9125ddb2f54cb6ced0cf1
toru <toru777average@cock.li>
2024-07-25 23:33:15 -0400

    added btop, nwg-look, obs-studio, qbitorrent and xsettingsd to dotfiles, modified tmount script, waybar and zshrc

--- .scripts/tmount.sh
+++ .scripts/tmount.sh
@@ -4,10 +4,10 @@
 # Created On: 25/07/24
 # Project: tombs scripts
 
-TOMB_DIR="$HOME/dc/tomb"
+TOMB_DIR="$HOME/dc/tomb/"
 KEY_FILE="$TOMB_DIR/secret.key"
 
-ACTION=$(echo -e "🕯️ Raise the dead (open)\n🪦 Rest in peace (close)" | fzf --prompt="Choose your action: ")
+ACTION=$(echo -e "🕯️ Raise the dead (open)\n🪦 Rest in peace (close)\n🔮 Tomb Slam All" | fzf --prompt="Choose your action: ")
 
 if [ "$ACTION" == "🕯️ Raise the dead (open)" ]; then
     TOMB_FILE=$(find "$TOMB_DIR" -type f -name "*.tomb" | fzf --prompt="Select a tomb to open: ")
@@ -20,6 +20,9 @@ if [ "$ACTION" == "🕯️ Raise the dead (open)" ]; then
 elif [ "$ACTION" == "🪦 Rest in peace (close)" ]; then
     echo "🔒 Sealing the tomb..."
     tomb close
+elif [ "$ACTION" == "🔮 Tomb Slam All" ]; then
+    echo "🔒 Slamming all tombs shut..."
+    tomb slam all
 else
     echo "No action selected. The spirits remain restless."
 fi
