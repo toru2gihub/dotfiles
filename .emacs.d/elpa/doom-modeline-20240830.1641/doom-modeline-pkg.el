3a4af27320be0a06c8b385b13f14c0a2005d97bb
toru <toru777average@cock.li>
2024-09-03 23:39:36 -0400

    update to emacs

---
+++ .emacs.d/elpa/doom-modeline-20240830.1641/doom-modeline-pkg.el
@@ -0,0 +1,17 @@
+(define-package "doom-modeline" "20240830.1641" "A minimal and modern mode-line"
+  '((emacs "25.1")
+    (compat "29.1.4.5")
+    (nerd-icons "0.1.0")
+    (shrink-path "0.3.1"))
+  :commit "aa5a4a60e525b9c269205f9062fb33cee335ffe2" :authors
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("faces" "mode-line")
+  :url "https://github.com/seagle0128/doom-modeline")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
